1ba7a9d460c088d136eaf98f88afe3b0ca9006ee
Aung Myo Kyaw <me@aungmyokyaw.com>
2016-10-13 08:20:44 +0000

    fixed heroku

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var express = require("express");
 var timeStampMs = express();
 var time = require("./time.js");
+var port = Number(process.env.PORT||8080);
 timeStampMs.get('/:time',function(req,res){
     var data = req.params;
     // console.log(data);
     res.json(time(data.time));
 });
 timeStampMs.use('/',express.static(__dirname+'/public'));
-timeStampMs.listen(8080,function(){
-    console.log('OK');
+timeStampMs.listen(port,function(){
+    console.log('Everything is OK');
 });
